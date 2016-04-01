73edda5b6d3eb4d73c7bfd0a81020f7f2c6c035d
Jan Gerhard Schøpp <jgschoepp@gmail.com>
2016-04-01 00:22:10 -0400

    Added custom toolbar
    Added teacher icon to toolbar

--- Tutor/app/src/main/java/com/example/jangerhard/tutor/MainActivity.java
+++ Tutor/app/src/main/java/com/example/jangerhard/tutor/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -21,6 +24,10 @@ import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
+    private Menu menu;
+
+
     private BeaconManager beaconManager;
     private Region region;
     private TutorTimeTable timeTable;
@@ -34,6 +41,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         timeTable = new TutorTimeTable(this);
 
@@ -193,6 +204,31 @@ public class MainActivity extends AppCompatActivity {
 //                beaconManager.startRanging(region);
 //            }
 //        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        this.menu = menu;
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+
+
+            case R.id.settings:
+                return true;
+
+
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
